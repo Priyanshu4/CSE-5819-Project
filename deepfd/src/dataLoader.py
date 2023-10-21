Working on fixing deepfd loss calculation so large array of pairwise similarities is not needed

deepfd/src/dataLoader.py
@@ -1,5 +1,5 @@
-__author__ = 'Tong Zhao'
-__email__ = 'tzhao2@nd.edu'
+__author__ = "Tong Zhao"
+__email__ = "tzhao2@nd.edu"
 
 import os
 import sys
@@ -12,70 +12,95 @@ from scipy.sparse import csr_matrix
 
 from src.utils import *
 from collections import defaultdict, Counter
+from src.similarity import GraphSimilarity
 
-class DataLoader():
-    """ data loader """
+
+class DataLoader:
+    """data loader"""
+
     def __init__(self, args, logger):
         self.ds = args.dataSet
         self.args = args
         self.logger = logger
-        self.file_paths = json.load(open(f'{args.config_dir}/{args.file_paths}'))
+        self.file_paths = json.load(open(f"{args.config_dir}/{args.file_paths}"))
         self.load_dataSet(args.dataSet)
 
     def load_dataSet(self, dataSet):
         ds = dataSet
-        graph_u2p_file = self.file_paths[ds]['graph_u2p']
-        graph_simi_file = self.file_paths[ds]['graph_u2u_simi']
-        labels_file = self.file_paths[ds]['labels']
+        graph_u2p_file = self.file_paths[ds]["graph_u2p"]
+        graph_simi_file = self.file_paths[ds]["graph_u2u_simi"]
+        labels_file = self.file_paths[ds]["labels"]
 
-        graph_u2p = pickle.load(open(graph_u2p_file, 'rb'))
-        labels = pickle.load(open(labels_file, 'rb'))
+        graph_u2p = pickle.load(open(graph_u2p_file, "rb"))
+        labels = pickle.load(open(labels_file, "rb"))
         graph_u2p[graph_u2p > 0] = 1
         graph_u2u = graph_u2p @ graph_u2p.T
         if os.path.isfile(graph_simi_file):
-            graph_simi = pickle.load(open(graph_simi_file, 'rb'))
-            self.logger.info('loaded similarity graph from cache')
+            graph_simi = pickle.load(open(graph_simi_file, "rb"))
+            self.logger.info("loaded similarity graph from cache")
         else:
-            graph_simi = np.zeros(np.shape(graph_u2u))
-            nz_entries = []
-            for i in range(np.shape(graph_u2u)[0]):
-                for j in range(i+1, np.shape(graph_u2u)[0]):
-                    nz_entries.append([i, j])
-            self.logger.info(f'Calculating user-user similarity graph, {len(nz_entries)} edges to go...')
-            sz = 1000
-            n_batch = math.ceil(len(nz_entries) / sz)
-            batches = np.array_split(nz_entries, n_batch)
-            pool = Pool()
-            results = pool.map(get_simi_single_iter, [(entries_batch, graph_u2p) for entries_batch in batches])
-            results = list(zip(*results))
-            row = np.concatenate(results[0])
-            col = np.concatenate(results[1])
-            dat = np.concatenate(results[2])
-            for x in range(len(row)):
-                graph_simi[row[x], col[x]] = dat[x]
-                graph_simi[col[x], row[x]] = dat[x]
-            pickle.dump(graph_simi, open(graph_simi_file, "wb"))
-            self.logger.info('Calculated user-user similarity and saved it for catch.')
+            print(np.arange(np.shape(graph_u2p)[0]))
+            try:
+                graph_simi = np.zeros(np.shape(graph_u2u))
+                nz_entries = []
+                for i in range(np.shape(graph_u2u)[0]):
+                    for j in range(i + 1, np.shape(graph_u2u)[0]):
+                        nz_entries.append([i, j])
+                self.logger.info(
+                    f"Calculating user-user similarity graph, {len(nz_entries)} edges to go..."
+                )
+                sz = 1000
+                n_batch = math.ceil(len(nz_entries) / sz)
+                batches = np.array_split(nz_entries, n_batch)
+                pool = Pool()
+                results = pool.map(
+                    get_simi_single_iter,
+                    [(entries_batch, graph_u2p) for entries_batch in batches],
+                )
+                results = list(zip(*results))
+                row = np.concatenate(results[0])
+                col = np.concatenate(results[1])
+                dat = np.concatenate(results[2])
+                for x in range(len(row)):
+                    graph_simi[row[x], col[x]] = dat[x]
+                    graph_simi[col[x], row[x]] = dat[x]
+                pickle.dump(graph_simi, open(graph_simi_file, "wb"))
+                self.logger.info(
+                    "Calculated user-user similarity and saved it for catch."
+                )
+            except np.core._exceptions._ArrayMemoryError as e:
+                print(
+                    "Cannot fit user similarity array in memory! "
+                    + f" See exception below:\n{str(e)}"
+                    + "\nWill compute similarities lazily instead."
+                )
+                graph_simi = GraphSimilarity(graph_u2u)
 
         assert len(labels) == np.shape(graph_u2p)[0] == np.shape(graph_u2u)[0]
-        if ds == 'alpha':
-            labeled_nodes = np.where(labels>=0)[0]
-            test_indexs_cls, val_indexs_cls, train_indexs_cls = self._split_data_cls_limited(labeled_nodes)
+        if ds == "alpha":
+            labeled_nodes = np.where(labels >= 0)[0]
+            (
+                test_indexs_cls,
+                val_indexs_cls,
+                train_indexs_cls,
+            ) = self._split_data_cls_limited(labeled_nodes)
         else:
-            test_indexs_cls, val_indexs_cls, train_indexs_cls = self._split_data_cls(len(labels))
+            test_indexs_cls, val_indexs_cls, train_indexs_cls = self._split_data_cls(
+                len(labels)
+            )
 
-        setattr(self, dataSet+'_train', np.arange(np.shape(graph_u2p)[0]))
-        setattr(self, dataSet+'_cls_test', test_indexs_cls)
-        setattr(self, dataSet+'_cls_val', val_indexs_cls)
-        setattr(self, dataSet+'_cls_train', train_indexs_cls)
-        setattr(self, dataSet+'_u2p', graph_u2p)
-        setattr(self, dataSet+'_u2u', graph_u2u)
-        setattr(self, dataSet+'_simi', graph_simi)
-        setattr(self, dataSet+'_labels', labels)
+        setattr(self, dataSet + "_train", np.arange(np.shape(graph_u2p)[0]))
+        setattr(self, dataSet + "_cls_test", test_indexs_cls)
+        setattr(self, dataSet + "_cls_val", val_indexs_cls)
+        setattr(self, dataSet + "_cls_train", train_indexs_cls)
+        setattr(self, dataSet + "_u2p", graph_u2p)
+        setattr(self, dataSet + "_u2u", graph_u2u)
+        setattr(self, dataSet + "_simi", graph_simi)
+        setattr(self, dataSet + "_labels", labels)
 
     def get_train(self):
         training_cps = defaultdict(list)
-        g_u2u = getattr(self, self.ds+'_u2u')
+        g_u2u = getattr(self, self.ds + "_u2u")
         n = np.shape(g_u2u)[0]
         for i in range(n):
             line = g_u2u[i].toarray().squeeze()
@@ -103,12 +128,12 @@ class DataLoader():
         train_size = num_nodes - (test_size + val_size)
 
         test_indexs = rand_indices[:test_size]
-        val_indexs = rand_indices[test_size:(test_size+val_size)]
-        train_indexs = rand_indices[(test_size+val_size):]
+        val_indexs = rand_indices[test_size : (test_size + val_size)]
+        train_indexs = rand_indices[(test_size + val_size) :]
 
         return test_indexs, val_indexs, train_indexs
 
-    def _split_data_cls_limited(self, nodes, test_split = 4, val_split = 4):
+    def _split_data_cls_limited(self, nodes, test_split=4, val_split=4):
         # used when only limited nodes are labeled
         np.random.shuffle(nodes)
 
@@ -117,7 +142,7 @@ class DataLoader():
         train_size = len(nodes) - (test_size + val_size)
 
         val_indexs = nodes[:test_size]
-        test_indexs = nodes[test_size:(test_size+val_size)]
-        train_indexs = nodes[(test_size+val_size):]
+        test_indexs = nodes[test_size : (test_size + val_size)]
+        train_indexs = nodes[(test_size + val_size) :]
 
         return test_indexs, val_indexs, train_indexs

deepfd/src/models.py
@@ -1,5 +1,5 @@
-__author__ = 'Tong Zhao'
-__email__ = 'tzhao2@nd.edu'
+__author__ = "Tong Zhao"
+__email__ = "tzhao2@nd.edu"
 
 import os
 import sys
@@ -11,6 +11,7 @@ from scipy.sparse import csr_matrix
 
 import torch.nn as nn
 import torch.nn.functional as F
+
 
 class Classification(nn.Module):
     def __init__(self, emb_size):
@@ -32,6 +33,7 @@ class Classification(nn.Module):
         x = F.elu_(self.fc2(x))
         logists = torch.log_softmax(x, 1)
         return logists
+
 
 class DeepFD(nn.Module):
     def __init__(self, features, feat_size, hidden_size, emb_size):
@@ -59,8 +61,11 @@ class DeepFD(nn.Module):
         recon = F.relu_(self.fc4(x_de))
         return embs, recon
 
-class Loss_DeepFD():
-    def __init__(self, features, graph_simi, device, alpha, beta, gamma):
+
+class Loss_DeepFD:
+    def __init__(
+        self, features, graph_simi, device, alpha, beta, gamma, negative_samples=10
+    ):
         self.features = features
         self.graph_simi = graph_simi
         self.device = device
@@ -70,6 +75,7 @@ class Loss_DeepFD():
         self.node_pairs = {}
         self.original_nodes_batch = None
         self.extended_nodes_batch = None
+        self.negative_samples = negative_samples
 
     def extend_nodes(self, nodes_batch, training_cps):
         self.original_nodes_batch = copy.deepcopy(nodes_batch)
@@ -93,7 +99,7 @@ class Loss_DeepFD():
         return loss
 
     def get_loss_simi(self, embs_batch):
-        node2index = {n:i for i,n in enumerate(self.extended_nodes_batch)}
+        node2index = {n: i for i, n in enumerate(self.extended_nodes_batch)}
         simi_feat = []
         simi_embs = []
         for node, cps in self.node_pairs.items():