c9da8c5c60cdc46991b67cca6c6ff90ea07e63f3
nnsaravanan <wowniteesh@gmail.com>
2023-11-03 17:48:49 -0400

    added test threads

--- multiprocessing/testmp.py
+++ multiprocessing/testmp.py
@@ -9,11 +9,13 @@ def f(x):
     return x*x
 
 if __name__ == '__main__':
+    l = range(240000000)
+    
     with Pool(num_cores) as p:
         start = time.time()
-        result = p.map(f, range(240000000))
+        result = p.map(f, l)
         print(time.time() - start)
     
     start = time.time()
-    result = list(map(f, range(240000000)))
+    result = list(map(f, l))
     print(time.time() - start)
