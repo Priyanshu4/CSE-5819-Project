6faca801b39a7ef0dbb52a2ec1994538b5a95859
Priyanshu Agrawal <priyanshu.agrawal@uconn.edu>
2023-11-04 11:08:41 -0400

    Fixed DeepFD slurm job

---
+++ jobs/deepfd_yelpnyc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=general
+#SBATCH --ntasks=24
+#SBATCH --constraint='skylake'
+#SBATCH --nodes=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=priyanshu.agrawal@uconn.edu      # Destination email address
+#SBATCH --mem=20G
+
+module purge
+
+source /home/pra20003/miniconda3/etc/profile.d/conda.sh
+
+conda activate deepfd-env
+
+cd ../deepfd
+
+python -m src.main --cuda -1 --dataSet yelpnyc --cls_method dbscan
