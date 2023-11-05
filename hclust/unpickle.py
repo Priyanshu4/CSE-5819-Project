d0d9a54f1aea75555eb54845ba0faf1a4eae8d43
nnsaravanan <wowniteesh@gmail.com>
2023-11-05 17:46:13 -0500

    check embedded

--- hclust/unpickle.py
+++ hclust/unpickle.py
@@ -1,17 +1,17 @@
 import numpy as np
 import pickle
 
-def unpickle_and_print(file_name):
+def unpickle(file_name):
     try:
         with open(file_name, "rb") as file:
             loaded_object = pickle.load(file)
             print("Successfully loaded the pickled object:")
-            print(loaded_object)
+            return loaded_object
     except FileNotFoundError:
         print(f"File '{file_name}' not found.")
     except Exception as e:
         print(f"An error occurred while loading the pickled object: {e}")
 
 # Example usage:
-file_name = "hclust/results/2023-11-05_00-57-23/leaves.pkl"
-unpickle_and_print(file_name)
+file = "/Users/niteeshsaravanan/Documents/GitHub/CSE-5819-Project/data/yelpnyc/embedded/deepfd/embs_ep10.pkl"
+obj = unpickle(file)
