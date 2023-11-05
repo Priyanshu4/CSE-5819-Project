9d40f1b4d10d31f9ff265a5063f3392470023eea
nnsaravanan <wowniteesh@gmail.com>
2023-11-05 17:51:34 -0500

    updated path

--- hclust/unpickle.py
+++ hclust/unpickle.py
@@ -13,7 +13,7 @@ def unpickle(file_name):
         print(f"An error occurred while loading the pickled object: {e}")
 
 # Example usage:
-file = "/Users/niteeshsaravanan/Documents/GitHub/CSE-5819-Project/data/yelpnyc/embedded/deepfd/embs_ep10.pkl"
+file = "../data/yelpnyc/embedded/deepfd/embs_ep10.pkl"
 obj = unpickle(file)
 print(len(obj))
 print(type(obj))
