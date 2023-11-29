ff42f8343d1e90b1e503c13089bbfde42f7420ed
Priyanshu Agrawal <priyanshu.agrawal@uconn.edu>
2023-11-29 17:25:44 -0500

    Added jobs

---
+++ jobs/yelpnyc_embed_hclust_cpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=general
+#SBATCH --ntasks=12
+#SBATCH --nodes=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=priyanshu.agrawal+hpc@uconn.edu      
+#SBATCH --mem=16G
+#SBATCH --job-name=hclust
+
+module purge
+
+source /home/pra20003/miniconda3/etc/profile.d/conda.sh
+
+conda activate fake-review-detection
+
+cd ..
+
+python -m src.main --name hclust_test1 --dataset yelpnyc --loss simi --optimizer adam --epochs 100 --dim 4 --clustering hclust
