814333e34b2db5e8959ddd4922a6976dc7d59d24
Priyanshu Agrawal <priyanshu.agrawal@uconn.edu>
2023-11-29 20:30:18 -0500

    Minor changes to sbatch job

--- jobs/yelpnyc_embed_hclust_cpu.sh
+++ jobs/yelpnyc_embed_hclust_cpu.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=priyanshu.agrawal+hpc@uconn.edu      
 #SBATCH --mem=16G
-#SBATCH --job-name=hclust
+#SBATCH --job-name=yelpnyc_16d_hclust
 
 module purge
 
@@ -16,4 +16,4 @@ conda activate fake-review-detection
 
 cd ..
 
-python -m src.main --name hclust_test1 --dataset yelpnyc --loss simi --optimizer adam --epochs 100 --dim 4 --clustering hclust
+python -m src.main --name yelpnyc_16d_hclust --dataset yelpnyc --loss simi --optimizer adam --epochs 100 --dim 16 --clustering hclust
